bef61e1d1cf312b892f517a1736fa963a511de4b
Beamdamon <Beamdamon@gmail.com>
2023-01-15 12:57:53 -0700

    Added Stack Menu

--- Test Driven Development and Performance Evaluation.cpp
+++ Test Driven Development and Performance Evaluation.cpp
@@ -5,5 +5,9 @@ using namespace std;
 
 int main()
 {
-
+	cout << "Stack Menu" << endl;
+	cout << "1 = display" << endl;
+	cout << "2 = push" << endl;
+	cout << "3 = pop" << endl;
+	cout << "4 = exit" << endl;
 }
