0e7515e00b13741c1d5d15f58c50649ac699fee0
Beamdamon <Beamdamon@gmail.com>
2023-01-15 13:20:49 -0700

    Revised stack variables

--- Test Driven Development and Performance Evaluation.cpp
+++ Test Driven Development and Performance Evaluation.cpp
@@ -1,15 +1,19 @@
 
 #include <iostream>
-#include <stack>
 using namespace std;
+
+int stack[25], top;
+void push(int[], int);
+void pop(int[]);
+void display(int[]);
 
 int main()
 {
-	stack<int> stack;
 	int choice, item = 0;
 	cout << "Stack Menu" << endl;
 	cout << "1 = display" << endl;
 	cout << "2 = push" << endl;
 	cout << "3 = pop" << endl;
 	cout << "4 = exit" << endl;
+	cin >> choice;
 }
